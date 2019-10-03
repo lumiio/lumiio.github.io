085d6c27777b1b66740d731c5a92d66e28594def
Linda <37957722+lumiio@users.noreply.github.com>
2019-10-03 09:57:00 -0400

    Updates

---
+++ precache-manifest.49fca03e8ab67124694388033b8c13fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "414a9377f851431166d4b05df96f13fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc095cba28ef2fcccbdb",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "83887daabf20b8277b91",
+    "url": "/static/js/2.35a8a4bc.chunk.js"
+  },
+  {
+    "revision": "bc095cba28ef2fcccbdb",
+    "url": "/static/js/main.9e302156.chunk.js"
+  },
+  {
+    "revision": "ea05154ad6f202982826",
+    "url": "/static/js/runtime-main.c5757aae.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
